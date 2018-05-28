61620beb12ed99d83d7da8eb11bc5fa160f850a9
Maxime.Dd <deniaud.maxime@gmail.com>
2018-05-28 12:28:27 +0200

    exo blog : conversion date en string avec formattage pour éviter les problèmes de stockage avec firebase

--- src/app/model/post.model.ts
+++ src/app/model/post.model.ts
@@ -2,12 +2,25 @@ export class PostModel {
   title: string;
   content: string;
   loveIts: number;
-  creationDate: Date;
+  creationDate: string;
 
   constructor(pTitle: string, pContent: string) {
     this.title = pTitle;
     this.content = pContent;
     this.loveIts = 0;
-    this.creationDate = new Date();
+    this.creationDate = this.dateNow();
+  }
+
+  dateNow() {
+    const dateNow = new Date();
+    return this.addZero(dateNow.getDay().toString()) + '/'
+      + this.addZero(dateNow.getMonth().toString()) + '/'
+      + this.addZero(dateNow.getFullYear().toString()) + ' : '
+      + this.addZero(dateNow.getHours().toString()) + 'h '
+      + this.addZero(dateNow.getMinutes().toString());
+  }
+
+  addZero(numString: string) {
+    return numString.length === 1 ? '0' + numString : numString;
   }
 }
