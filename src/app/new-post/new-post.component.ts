c0d4f7bc7ddd5f6b8cbea3a4d34c7849937cf304
Maxime.Dd <deniaud.maxime@gmail.com>
2018-05-28 12:29:32 +0200

    exo blog : composant pour l'ajout d'un post

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {PostService} from '../service/post.service';
+import {PostModel} from '../model/post.model';
 
 @Component({
   selector: 'app-new-post',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPostComponent implements OnInit {
 
-  constructor() { }
+  postForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private postService: PostService) {
   }
 
+  ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.postForm = this.formBuilder
+      .group({
+        title: ['', Validators.required],
+        content: ['', Validators.required]
+      });
+  }
+
+  onSavePost() {
+    const title = this.postForm.get('title').value;
+    const content = this.postForm.get('content').value;
+
+    const newPost = new PostModel(title, content);
+    this.postService.createPost(newPost);
+    this.router.navigate(['/posts']);
+  }
 }
