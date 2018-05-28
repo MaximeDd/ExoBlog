4ff18d14483fd16e990f920fb6647dab84b7ae15
Maxime.Dd <deniaud.maxime@gmail.com>
2018-05-28 12:27:42 +0200

    exo blog : suppression liste en dur

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -12,18 +12,6 @@ export class PostService {
   postSuject = new Subject<PostModel[]>();
 
   constructor() {
-    this.posts.push(new PostModel('Mon premier post', 'Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam ' +
-      'mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem, sed amicitia remissior esse ' +
-      'debet et liberior et dulcior et ad omnem comitatem facilitatemque proclivior.'));
-
-    this.posts.push(new PostModel('Mon deuxième post', 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati ' +
-      'recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ' +
-      'dissimulantem formidine tenus iusserim custodiri.'));
-
-    this.posts.push(new PostModel('Mon troisième post', 'Eodem tempore etiam Hymetii praeclarae indolis viri negotium est ' +
-      'actitatum, cuius hunc novimus esse textum. cum Africam pro consule regeret Carthaginiensibus victus inopia iam lassatis, ex ' +
-      'horreis Romano populo destinatis frumentum dedit, pauloque postea cum provenisset segetum copia, ' +
-      'integre sine ulla restituit mora.'));
   }
 
   emitPost() {
